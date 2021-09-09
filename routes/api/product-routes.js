c2cac26194f0347952d94b2419f1ae1bb53fcec8
Zachary Dowd <zacharyd2796@zohomail.com>
2021-09-09 17:02:14 -0600

    product delete route added

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -106,8 +106,25 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
-});
+// delete product by id
+router.delete('/:id', async (req, res) => {
+    try {
+      const productData = await Product.destroy({
+        where: {
+          id: req.params.id
+        }
+      });
+  
+      if (!productData) {
+        res.status(404).json({ message: 'No location found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(`Product ${req.params.id} has been deleted!`);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 
 module.exports = router;
