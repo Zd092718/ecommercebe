42ce6c998525a5760a842417f5105fe5ff6ab5a7
Zachary Dowd <zacharyd2796@zohomail.com>
2021-09-08 18:26:41 -0600

    reverted connection changes

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,16 @@
-const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-    process.env.DB_USER,
-    process.env.DB_PW,
-    process.env.DB_NAME,
-    {
+const Sequelize = require('sequelize');
+
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
       },
-      port: 3001
-    }
-);
+      port: 3306
+    });
 
 module.exports = sequelize;
